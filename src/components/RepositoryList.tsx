447e8dd6225ba16cb1f524240f190b741aac24a5
Neto Veloso <nettoveloso@gmail.com>
2021-04-05 22:15:08 -0300

    First Project Typescript and React

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+interface Repository {
+  id: string;
+  name: string;
+  html_url: string;
+  description: string;
+}
+
+export function RepositoryList(){
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() =>{
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  },[]);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        {
+          repositories.map( repository => {
+            return <RepositoryItem key={repository.id} repository={repository} />  
+          })
+        }
+      </ul>
+    </section>
+  )
+}
